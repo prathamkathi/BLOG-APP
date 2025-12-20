5085e3b35bfec586a8b535970d0df964530c2d20
prathamkathi <pratham.kathi786@gmail.com>
2025-12-20 19:10:39 +0530

    blog app setup

---
+++ README.md
@@ -0,0 +1,3 @@
+check out all the dependencies in package.json
+
+ADD THIS LINE TO NOTES: import.meta.env.VITE_APPWRITE_URL
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+console.log(import.meta.env.VITE_APPWRITE_URL);
+function App() {
+  // console.log(process.env.VITE_APPWRITE_URL);
+  return <></>;
+}
+
+export default App;
